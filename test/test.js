dee68c448ecf8f8096d2b181f5c6c7278bea302c
errorrik <errorrik@gmail.com>
2018-08-09 20:32:27 +0800

    add test tool and script

---
+++ test/test.js
@@ -0,0 +1,10 @@
+var chai = require('chai');
+var expect = chai.expect;
+
+
+describe('Export Object', function() {
+    it('is an Object', function() {
+        expect(jsNative).to.be.a('object');
+    });
+});
+
