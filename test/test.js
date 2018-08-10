0ba4744b36ee8dc9f96a2721f4d1f82c00eecb7a
errorrik <errorrik@gmail.com>
2018-08-10 16:23:24 +0800

    add specs

--- test/test.js
+++ test/test.js
@@ -1,10 +1,32 @@
-var chai = require('chai');
-var expect = chai.expect;
+const chai = require('chai');
+const expect = chai.expect;
 
 
-describe('Export Object', function() {
-    it('is an Object', function() {
-        expect(jsNative).to.be.a('object');
+describe('Export Object', () => {
+    it('is an apiContainer = ', () => {
+        expect(jsNative).to.be.an('object');
+        expect(jsNative.add).to.be.a('function');
+        expect(jsNative.map).to.be.a('function');
+        expect(jsNative.invoke).to.be.a('function');
+        expect(jsNative.fromNative).to.be.a('function');
+    });
+
+    it('has invokeAPI method', () => {
+        expect(jsNative.invokeAPI).to.be.a('function');
+    });
+
+    it('has createContainer method', () => {
+        expect(jsNative.createContainer).to.be.a('function');
     });
 });
 
+
+describe('APIContainer', () => {
+    it('instancing by createContainer', () => {
+        let apiContainer = jsNative.createContainer();
+        expect(apiContainer.add).to.be.a('function');
+        expect(apiContainer.map).to.be.a('function');
+        expect(apiContainer.invoke).to.be.a('function');
+        expect(apiContainer.fromNative).to.be.a('function');
+    });
+});
