0a1cc12bb9cff0cdd2c1e1d4c0485aae71c90963
errorrik <errorrik@gmail.com>
2018-08-10 17:16:41 +0800

    add args pass specs

--- test/test.js
+++ test/test.js
@@ -42,16 +42,59 @@ describe('APIContainer', () => {
         let invoked = false;
 
         apis.add({
-            "invoke": "method",
-            "name": "api1",
-            "method": "tAPI.api1"
+            invoke: "method",
+            name: "api1",
+            method: "tAPI.api1"
         });
         tAPI.api1 = () => {
             invoked = true;
         };
+
         apis.invoke('api1');
-
-
         expect(invoked).to.be.true;
     });
+
+    it('invoke a not defined api, no error, and return undefined', () => {
+        let value = apis.invoke('api-noexists');
+        expect(value).to.be.undefined;
+    });
+
+
+    it('in args declaration, arg passed', () => {
+        let sum = 0;
+
+        apis.add({
+            invoke: ["CallMethod"],
+            name: "api3",
+            method: "tAPI.api3",
+            args: [
+                {name: 'one', type: 'number'},
+                {name: 'two', type: 'number'}
+            ]
+        });
+        tAPI.api3 = (one, two) => {
+            sum = one + two;
+        };
+
+        apis.invoke('api3', [1, 2, 3]);
+        expect(sum).to.be.equal(3);
+    });
+
+    it('out of args declaration, arg also pass', () => {
+        let valid = false;
+
+        apis.add({
+            invoke: ["CallMethod"],
+            name: "api2",
+            method: "tAPI.api2"
+        });
+        tAPI.api2 = arg => {
+            valid = arg;
+        };
+
+        apis.invoke('api2', [1, 2, 3]);
+        expect(valid).to.be.not.false;
+    });
+
+
 });
